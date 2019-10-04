5e940d6a7822ac88e0ec6c693999f9e0a2a4126f
Raúl Moreno <45350116+Shadier@users.noreply.github.com>
2019-10-04 02:45:45 -0500

    ey

---
+++ precache-manifest.a2ccd40ffb10c7e6adef3ea8cfdbb9d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123ff67fe950b384c5ff307d668d7eca",
+    "url": "/herreria.github.io/index.html"
+  },
+  {
+    "revision": "ac9d006c5e4921cfcc60",
+    "url": "/herreria.github.io/static/css/main.0e8f39ff.chunk.css"
+  },
+  {
+    "revision": "8f9f9f703c671000de7b",
+    "url": "/herreria.github.io/static/js/2.3e91e924.chunk.js"
+  },
+  {
+    "revision": "ac9d006c5e4921cfcc60",
+    "url": "/herreria.github.io/static/js/main.a47770ca.chunk.js"
+  },
+  {
+    "revision": "1461e1e3ab703a9d388f",
+    "url": "/herreria.github.io/static/js/runtime~main.8fd4863a.js"
+  },
+  {
+    "revision": "eb219d0defd6f4f9f6fbb16bace29123",
+    "url": "/herreria.github.io/static/media/LOGO.eb219d0d.png"
+  },
+  {
+    "revision": "a4f2cb80ff2ae2772e80bf30e9d78d4c",
+    "url": "/herreria.github.io/static/media/loading.a4f2cb80.gif"
+  },
+  {
+    "revision": "9d378898517e631cb3c02db6e3920a2c",
+    "url": "/herreria.github.io/static/media/wallpaper.9d378898.jpg"
+  }
+]);
